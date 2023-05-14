93eb0c31edfa281bbaeb234a30404ef1e85f877e
Cléo Rebert <cleo.rebert@gmail.com>
2023-05-14 14:22:38 +0200

    Replace more Direction with Self
    Signed-off-by: Cléo Rebert <cleo.rebert@gmail.com>

--- src/direction.rs
+++ src/direction.rs
@@ -17,12 +17,12 @@ impl From<i32> for Direction {
             val = 5;
         }
         match (val % 6).abs() {
-            0 => Direction::TopRight,
-            1 => Direction::Right,
-            2 => Direction::BottomRight,
-            3 => Direction::BottomLeft,
-            4 => Direction::Left,
-            5 => Direction::TopLeft,
+            0 => Self::TopRight,
+            1 => Self::Right,
+            2 => Self::BottomRight,
+            3 => Self::BottomLeft,
+            4 => Self::Left,
+            5 => Self::TopLeft,
             _ => unreachable!(),
         }
     }
@@ -31,12 +31,12 @@ impl From<i32> for Direction {
 impl Into<i32> for Direction {
     fn into(self) -> i32 {
         match self {
-            Direction::TopRight => 0,
-            Direction::Right => 1,
-            Direction::BottomRight => 2,
-            Direction::BottomLeft => 3,
-            Direction::Left => 4,
-            Direction::TopLeft => 5,
+            Self::TopRight => 0,
+            Self::Right => 1,
+            Self::BottomRight => 2,
+            Self::BottomLeft => 3,
+            Self::Left => 4,
+            Self::TopLeft => 5,
         }
     }
 }
@@ -44,12 +44,12 @@ impl Into<i32> for Direction {
 impl std::fmt::Display for Direction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Direction::TopRight => f.write_str("↗"),
-            Direction::Right => f.write_str("→"),
-            Direction::BottomRight => f.write_str("↘"),
-            Direction::BottomLeft => f.write_str("↙"),
-            Direction::Left => f.write_str("←"),
-            Direction::TopLeft => f.write_str("↖"),
+            Self::TopRight => f.write_str("↗"),
+            Self::Right => f.write_str("→"),
+            Self::BottomRight => f.write_str("↘"),
+            Self::BottomLeft => f.write_str("↙"),
+            Self::Left => f.write_str("←"),
+            Self::TopLeft => f.write_str("↖"),
         }
     }
 }
